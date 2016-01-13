758e1e4572db1ee93b6bdc220544215b8341eb16
Paul <pfultz2@yahoo.com>
2016-01-13 17:58:45 -0600

    Add initial cmake

---
+++ KokkosCore_config.h
@@ -0,0 +1,10 @@
+/* ---------------------------------------------
+Makefile constructed configuration:
+Wed Sep  2 13:41:22 CDT 2015
+----------------------------------------------*/
+/* Execution Spaces */
+#define KOKKOS_HAVE_SERIAL 1
+#define KOKKOS_HAVE_KALMAR 1
+/* General Settings */
+#define KOKKOS_HAVE_CXX11 1
+/* Cuda Settings */
